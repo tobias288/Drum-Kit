b74de1cfe04810c6bafc16ccfb3363d835156465
Tobias Uchechukwu <tuchechukwu@unionbankng.com>
2022-10-02 15:08:25 +0100

    Completed Drum-Kit Challenge

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    alert("I got clicked!");
+  });
+
+}
